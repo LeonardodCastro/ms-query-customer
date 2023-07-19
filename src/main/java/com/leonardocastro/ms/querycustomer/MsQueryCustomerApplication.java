f9c63609e72395e05d0d3abe79c6f3ccb6397b39
leonardodecastrodasilveira <dev.leonardocastro@gmail.com>
2023-07-19 09:29:19 -0300

    project created. Docker and properties set up

---
+++ src/main/java/com/leonardocastro/ms/querycustomer/MsQueryCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.leonardocastro.ms.querycustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsQueryCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsQueryCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardocastro/ms/querycustomer/MsQueryCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardocastro.ms.querycustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsQueryCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
