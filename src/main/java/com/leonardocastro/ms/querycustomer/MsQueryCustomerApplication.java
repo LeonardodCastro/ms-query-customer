84842e944b1ab8a9154911ee5a6ce308d5bb8ef0
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-07-31 21:39:02 -0300

    Habilitando swagger

--- src/main/java/com/leonardocastro/ms/querycustomer/MsQueryCustomerApplication.java
+++ src/main/java/com/leonardocastro/ms/querycustomer/MsQueryCustomerApplication.java
@@ -2,8 +2,10 @@ package com.leonardocastro.ms.querycustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MsQueryCustomerApplication {
 
 	public static void main(String[] args) {
