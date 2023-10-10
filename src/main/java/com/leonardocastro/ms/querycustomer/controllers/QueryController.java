6955d91bf2f6fe1f2eafc44f998bb36451dd5c64
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-10 15:12:04 -0300

    update endpoint

--- src/main/java/com/leonardocastro/ms/querycustomer/controllers/QueryController.java
+++ src/main/java/com/leonardocastro/ms/querycustomer/controllers/QueryController.java
@@ -1,6 +1,7 @@
 package com.leonardocastro.ms.querycustomer.controllers;
 
-import com.leonardocastro.ms.querycustomer.dtos.RequestDTO;
+import com.leonardocastro.ms.querycustomer.dtos.PostRequest;
+import com.leonardocastro.ms.querycustomer.dtos.UpdateRequestDTO;
 import com.leonardocastro.ms.querycustomer.services.FindByIdCustomerService;
 import com.leonardocastro.ms.querycustomer.services.FindCustomerService;
 import com.leonardocastro.ms.querycustomer.services.RegisterCustomerService;
@@ -29,8 +30,12 @@ public class QueryController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> saveCustomer(@RequestBody RequestDTO requestDTO) {
-        return ResponseEntity.ok(registerCustomerService.saveCustomer(requestDTO));
+    public ResponseEntity<?> saveCustomer(@RequestBody PostRequest postRequest) {
+        return ResponseEntity.ok(registerCustomerService.saveCustomer(postRequest));
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCustomer(@PathVariable Long id, @RequestBody UpdateRequestDTO updateRequestDTO){
+        return ResponseEntity.ok(registerCustomerService.updateCustomerById(id, updateRequestDTO));
     }
 
 }
