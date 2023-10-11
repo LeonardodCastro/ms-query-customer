c7b03e6e3022d2917499a904e8954ed3e00b86a4
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-11 09:41:17 -0300

    adding new enum error

--- src/main/java/com/leonardocastro/ms/querycustomer/enums/Errors.java
+++ src/main/java/com/leonardocastro/ms/querycustomer/enums/Errors.java
@@ -2,7 +2,9 @@ package com.leonardocastro.ms.querycustomer.enums;
 
 
 public enum Errors {
-    QC001("QC-001", "Customer [%d] does not exist");
+    QC201("QC-201","Invalid field(s)"),
+    QC101("QC-101", "Customer [%d] does not exist");
+
     private final String errorCode;
     private final String message;
      Errors(String errorCode, String message){
