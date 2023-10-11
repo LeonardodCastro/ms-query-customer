1bd50a2d1654fb5b6739278cf4a9a91448f48d8a
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-11 09:42:39 -0300

    adding MethodArgumentNotValidException

--- src/main/java/com/leonardocastro/ms/querycustomer/exceptions/ControllerAdvice.java
+++ src/main/java/com/leonardocastro/ms/querycustomer/exceptions/ControllerAdvice.java
@@ -2,10 +2,15 @@ package com.leonardocastro.ms.querycustomer.exceptions;
 
 
 import com.leonardocastro.ms.querycustomer.controllers.response.ErrorResponse;
+import com.leonardocastro.ms.querycustomer.controllers.response.FieldErrorResponse;
+import com.leonardocastro.ms.querycustomer.enums.Errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.stream.Collectors;
 
 @org.springframework.web.bind.annotation.ControllerAdvice
 public class ControllerAdvice {
@@ -18,5 +23,17 @@ public class ControllerAdvice {
                 null
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> argumentNotValidException(MethodArgumentNotValidException ex, WebRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                Errors.QC201.getMessage(),
+                Errors.QC201.getErrorCode(),
+                ex.getBindingResult().getFieldErrors().stream()
+    .map(fieldError -> new FieldErrorResponse(fieldError.getDefaultMessage(),(fieldError.getField())))
+    .collect(Collectors.toList()));
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
