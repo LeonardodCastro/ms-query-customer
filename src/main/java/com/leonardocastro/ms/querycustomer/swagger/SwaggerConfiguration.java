7e8162418ede1e8b5571930bc6fb6f2993154dd3
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-11 10:25:18 -0300

    Adding tittle and description

--- src/main/java/com/leonardocastro/ms/querycustomer/swagger/SwaggerConfiguration.java
+++ src/main/java/com/leonardocastro/ms/querycustomer/swagger/SwaggerConfiguration.java
@@ -2,8 +2,10 @@ package com.leonardocastro.ms.querycustomer.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,6 +17,10 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("ms-query-customer")
+                        .description("this microservice is able to query zip code in an external api and register customers in database")
+                        .build());
     }
 }
