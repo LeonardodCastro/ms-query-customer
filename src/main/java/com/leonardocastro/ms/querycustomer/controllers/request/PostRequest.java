68dcf09e3ef66482b908c7f256609d061321dede
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-11 09:40:54 -0300

    adding valitations

--- src/main/java/com/leonardocastro/ms/querycustomer/controllers/request/PostRequest.java
+++ src/main/java/com/leonardocastro/ms/querycustomer/controllers/request/PostRequest.java
@@ -1,9 +1,17 @@
 package com.leonardocastro.ms.querycustomer.controllers.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public record PostRequest(
+        @NotBlank(message = "Name is mandatory")
         String name,
+
+        @NotNull(message = "Age is mandatory")
         Integer age,
+        @NotBlank(message = "Country code is mandatory")
         String countryCode,
+        @NotBlank(message = "Zip code is mandatory")
         String zip
 ) {
 }
