de24f7ba6521e84850719b2a195a3240e9c9c32f
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-11-06 18:49:41 -0300

    saveCustomer_03

--- src/test/java/com/leonardocastro/ms/querycustomer/controllers/QueryControllerTest.java
+++ src/test/java/com/leonardocastro/ms/querycustomer/controllers/QueryControllerTest.java
@@ -117,6 +117,11 @@ class QueryControllerTest {
         ResponseEntity<?> response = queryController.saveCustomer(postRequest);
         Assertions.assertNotEquals(expectedResponse, response);
     }
+    @Test
+    @DisplayName("Not throwing Exception")
+    void saveCustomer_03() throws Exception {
+        assertDoesNotThrow(() -> queryController.saveCustomer(postRequest));
+    }
 
     @Test
     void updateCustomer() {
