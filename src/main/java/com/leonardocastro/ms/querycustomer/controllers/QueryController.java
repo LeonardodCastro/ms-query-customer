d33264c8cf439cdf099d6f9b75161f4a9fbf106c
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-11-08 21:36:51 -0300

    refactoring

--- src/main/java/com/leonardocastro/ms/querycustomer/controllers/QueryController.java
+++ src/main/java/com/leonardocastro/ms/querycustomer/controllers/QueryController.java
@@ -2,12 +2,15 @@ package com.leonardocastro.ms.querycustomer.controllers;
 
 import com.leonardocastro.ms.querycustomer.controllers.request.PostRequest;
 import com.leonardocastro.ms.querycustomer.controllers.request.UpdateRequest;
+import com.leonardocastro.ms.querycustomer.entities.CustomerEntity;
 import com.leonardocastro.ms.querycustomer.exceptions.NotFoundException;
 import com.leonardocastro.ms.querycustomer.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
@@ -16,8 +19,9 @@ public class QueryController {
     CustomerService customerService;
 
     @GetMapping("/")
-    public ResponseEntity<?> findAllCustomers() {
-        return ResponseEntity.ok(customerService.findAllCustomer());
+    public ResponseEntity<List<CustomerEntity>> findAllCustomers() {
+        List<CustomerEntity> allCustomer = customerService.findAllCustomer();
+        return  ResponseEntity.ok(allCustomer);
     }
 
     @GetMapping("/{id}")
