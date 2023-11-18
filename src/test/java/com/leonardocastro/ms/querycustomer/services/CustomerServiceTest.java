b14499251d780ac696ebe1484a2f980a41b753d6
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-11-18 10:10:53 -0300

    creating CustomerServiceTest

---
+++ src/test/java/com/leonardocastro/ms/querycustomer/services/CustomerServiceTest.java
@@ -0,0 +1,33 @@
+package com.leonardocastro.ms.querycustomer.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAllCustomer() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void saveCustomer() {
+    }
+
+    @Test
+    void updateCustomerById() {
+    }
+
+    @Test
+    void deleteCustomerById() {
+    }
+}
